4162fb1bcf3b0f39c2e77e9493fc8fb4138f2337
Rodrigo Lima <rodrigotenorio2005@gmail.com>
2025-05-08 10:33:01 -0300

    Adicionando o Front-end do projeto no arquivo

---
+++ front-end/js/functions-nav.js
@@ -0,0 +1,60 @@
+
+//isso é pra pegar a nav do site e colocar lá
+fetch('../html/nav.html')
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById('navbar-placeholder').innerHTML = data;
+    
+          // pegando o nome do arquivo atual
+          const path = window.location.pathname.split("/").pop();
+          
+    
+          // adicionando a classe 'active' no link correspondente
+          if(path === "principal.html") {
+            document.getElementById("buttons-nav").classList.add("active");
+            
+          } 
+          else if (path === "metas.html") {
+            document.getElementById("buttons-nav-2").classList.add("active");
+            
+          } 
+          else if (path === "dicas.html") {
+            document.getElementById("buttons-nav-3").classList.add("active");
+          }
+        });
+
+
+
+
+
+/* =-=-=-=-=-=-=-=- JS do switch =-=-=-=-=-=-=-=- */
+    // controle do toggle
+    const notificationToggle = document.getElementById('notificationToggle');
+    const notificationToggleMobile = document.getElementById('notificationToggleMobile');
+
+    // sincroniza os tongos
+    function syncNotificationToggles() {
+      notificationToggleMobile.checked = notificationToggle.checked;
+    }
+
+    notificationToggle.addEventListener('change', syncNotificationToggles);
+    notificationToggleMobile.addEventListener('change', function() {
+      notificationToggle.checked = this.checked;
+    });
+
+    // se tiver estado, ele verifica 
+    if (localStorage.getItem('notificationsEnabled') !== null) {
+      const isEnabled = localStorage.getItem('notificationsEnabled') === 'true';
+      notificationToggle.checked = isEnabled;
+      notificationToggleMobile.checked = isEnabled;
+    }
+
+    // alterado o estado, ele salva
+    function saveNotificationPreference() {
+      localStorage.setItem('notificationsEnabled', notificationToggle.checked);
+      showAlert(`Notificações ${notificationToggle.checked ? 'ativadas' : 'desativadas'} com sucesso!`);
+    }
+
+    notificationToggle.addEventListener('change', saveNotificationPreference);
+    notificationToggleMobile.addEventListener('change', saveNotificationPreference);
+    //acabou graças a deus amém 
